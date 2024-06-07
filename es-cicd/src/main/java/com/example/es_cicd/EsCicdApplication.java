a3822f5b64d45eb3e3ee191950d3d654f0ae8a5b
myblan <myblanst@yahoo.com>
2024-06-07 03:24:34 -0700

    projet initial

---
+++ es-cicd/src/main/java/com/example/es_cicd/EsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.es_cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsCicdApplication.class, args);
+	}
+
+}
---
+++ es-cicd/src/test/java/com/example/es_cicd/EsCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.es_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
